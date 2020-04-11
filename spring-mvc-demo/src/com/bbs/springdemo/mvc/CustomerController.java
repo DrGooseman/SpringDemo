967aa42563ad329db4b4b0b2c82c0cfdd4360084
DrGooseman <jamesjquinn12@gmail.com>
2020-04-12 00:36:20 +0300

    finishes last name validation by trimming white space

--- spring-mvc-demo/src/com/bbs/springdemo/mvc/CustomerController.java
+++ spring-mvc-demo/src/com/bbs/springdemo/mvc/CustomerController.java
@@ -4,9 +4,12 @@ package com.bbs.springdemo.mvc;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -32,6 +35,13 @@ public class CustomerController {
 		}
 		
 	
+	}
+	
+	@InitBinder
+	public void initBinder(WebDataBinder dataBinder) {
+		
+		StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+		dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
 	}
 
 }
