9b196b58a854bff19338ffbe01757b7dd15282bc
DrGooseman <jamesjquinn12@gmail.com>
2020-05-04 22:08:47 +0300

    adds student class with hibernate annotations

---
+++ hibernate-tutorial/src/com/bbs/jdbc/TestJDBC.java
@@ -0,0 +1,27 @@
+package com.bbs.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class TestJDBC {
+
+	public static void main(String[] args) {
+		String jdbcUrl = "jdbc:mysql://localhost:3306/hb_student_tracker?useSSL=false&serverTimezone=UTC";
+		String user = "hbstudent";
+		String password = "hbstudent";
+		
+		System.out.println("Connecting to database: " + jdbcUrl);
+		
+		try {
+			Connection myConn = DriverManager.getConnection(jdbcUrl, user, password);
+			System.out.println("Connection successful!");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	}
+
+}
