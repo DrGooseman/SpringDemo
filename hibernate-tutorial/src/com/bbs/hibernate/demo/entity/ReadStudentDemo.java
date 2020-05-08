36b86e4c6fc9a12c5a2f8f97b19160eef72b07f3
DrGooseman <jamesjquinn12@gmail.com>
2020-05-08 21:56:50 +0300

    adds a get by ID demo

---
+++ hibernate-tutorial/src/com/bbs/hibernate/demo/entity/ReadStudentDemo.java
@@ -0,0 +1,49 @@
+package com.bbs.hibernate.demo.entity;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class ReadStudentDemo {
+
+	public static void main(String[] args) {
+
+		SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Student.class).buildSessionFactory();
+		
+		Session session = factory.getCurrentSession();
+		
+		try {
+		System.out.println("Creating new student object");
+		Student tempStudent = new Student("Sally", "Butterball", "Sally@email.com");
+		
+		session.beginTransaction();
+		
+		System.out.println("Saving the student...");
+		System.out.println(tempStudent);
+		session.save(tempStudent);
+		
+		session.getTransaction().commit();
+		
+		
+		System.out.print("Saved student. Generated id: " + tempStudent.getId());
+		
+		session = factory.getCurrentSession();
+		session.beginTransaction();
+		
+		System.out.println("\nGetting student with id: " + tempStudent.getId());
+		
+		Student myStudent = session.get(Student.class, tempStudent.getId());
+		
+		System.out.println("Get complete: " + myStudent);
+		
+		session.getTransaction().commit();
+		
+		System.out.println("Done!");
+		}
+		finally {
+			factory.close();
+		}
+		
+	}
+
+}
