74bc59682bbabc730e1c21fb1ddc2f878d740083
DrGooseman <jamesjquinn12@gmail.com>
2020-05-09 23:27:13 +0300

    adds update student (fixed)

--- hibernate-tutorial/src/com/bbs/hibernate/demo/entity/UpdateStudentDemo.java
+++ hibernate-tutorial/src/com/bbs/hibernate/demo/entity/UpdateStudentDemo.java
@@ -9,32 +9,21 @@ public class UpdateStudentDemo {
 	public static void main(String[] args) {
 
 		SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Student.class).buildSessionFactory();
-		
 		Session session = factory.getCurrentSession();
 		
 		try {
-		System.out.println("Creating new student object");
-		Student tempStudent = new Student("Sally", "Butterball", "Sally@email.com");
+			int studentId = 1;
+		
+		
 		
 		session.beginTransaction();
 		
-		System.out.println("Saving the student...");
-		System.out.println(tempStudent);
-		session.save(tempStudent);
+		System.out.println("\nGetting student with id: " + studentId);
 		
-		session.getTransaction().commit();
+		Student myStudent = session.get(Student.class, studentId);
 		
-		
-		System.out.print("Saved student. Generated id: " + tempStudent.getId());
-		
-		session = factory.getCurrentSession();
-		session.beginTransaction();
-		
-		System.out.println("\nGetting student with id: " + tempStudent.getId());
-		
-		Student myStudent = session.get(Student.class, tempStudent.getId());
-		
-		System.out.println("Get complete: " + myStudent);
+		System.out.println("Updating student...");
+		myStudent.setFirstName("Scooby..");
 		
 		session.getTransaction().commit();
 		
